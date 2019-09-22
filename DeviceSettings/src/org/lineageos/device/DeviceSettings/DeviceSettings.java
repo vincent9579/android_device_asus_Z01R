93ee6a26d8e25510066a53a71036988af3e94dcc
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-22 17:50:43 +0530

    [SQUASH] Z01R: Introduce DeviceSettings
    - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/f55c0218951d9b0f6586bdf5041a64ea14c96460/DeviceSettings
    - Adapted to AndroidX
    - Includes all translations from pie cycle
    - Dropped DC-Dimming QS Tile
    
    Change-Id: I7dd192534830ad927eafe83e77a9f7954095c75a
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    
    Z01R: DeviceSettings: Hide start margin of seek bar preference
    
    Also fix layout of preference
    
    Change-Id: I5758f3f76750d2e817bee003c616384eb575e154
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    
    Z01R: DeviceSettings: KeyHandler: Reduce log spam
    
    Change-Id: Ifadec33d2f66533ce2ddd15799af0a7be7d4f841
    
    Z01R: DeviceSettings: Add TouchGestures category
    
    Change-Id: I0fbb0a5d9bf549635ca24924f2eca04daecc7756
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Z01R: DeviceSettings: Dutch translations
    
    Change-Id: Ie0f0e6a607fca1c75a419f663f59a97307131dc9
    
    Z01R: DeviceSettings: Add a scrollable image view layout
    
    - from Havoc-Devices/android_device_oneplus_settings@720debd
    
    Derpfest edits:
    - Migrate to androidx
    
    EvoX edits:
    - Adapt to DeviceSettings
    - Swap display mode preview images 1 & 2
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Z01R: DeviceSettings: Add LogTile
    
    - Requires root
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Nuke Display Modes from DeviceSettings
    
    We only have WCG, which is enabled by default
    
    Z01R: DeviceSettings: Introduce FPS overlay
    
    Z01R: DeviceSettings: Add FPS overlay QS tile
    
    Z01R: DeviceSettings: Digital-style icon for FPSInfo QS Tile
    
    Z01R: DeviceSettings: Adapt FPS Info based on exposed data from kernel
    
    Also, cleanup panelsettings leftovers
    
    DeviceSettings: Adapt FPS for Z01R
    
    Z01R: DeviceSettings: Add FPS Info in DeviceSettings
    
    and comments cleanup
    
    Z01R: DeviceSettings: Introduce doze menu
    
    - This just launches the same activity as Settings>Display>Lock screen display>advanced settings does.
    - Since we're launching via DeviceSettings, drop config_customDozePackage in overlay
    
    Co-authored-by: AnierinB <anierinb@evolution-x.org>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Z01R: doze: Add Doze tile
    
    - Launches DozeSettingsActivity
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Z01R: DeviceSettings: Add missing drawables and layouts from OnePlus
    
    Co-authored-by: Anierin Bliss <anierinb@evolution-x.org>
    Signed-off-by: Aryan Arora <arora.aryan7@gmail.com>
    
    Z01R: DeviceSettings: Bump version to 2.0
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Z01R: DeviceSettings: We are now Device Extras!
    
    - We've gone far from what extras Asus includes in ZenUI. Rename to Device Extras as it sounds more appropriate.
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    Add support for multiple vibration intensities
    
    Oneplus => Asus
    
    Thanks Anierin for OnePlus settings (much lob)
    
    Z01R: DeviceSettings: FPSInfoService: Switch to isDreaming check
    
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

---
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettings.java
@@ -0,0 +1,108 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceSettings;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.util.Log;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.TwoStatePreference;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.device.DeviceSettings.FileUtils;
+
+public class DeviceSettings extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    public static final String KEY_VIBSTRENGTH = "vib_strength";
+    private VibratorStrengthPreference mVibratorStrength;
+    public static final String KEY_CALL_VIBSTRENGTH = "vib_call_strength";
+    private VibratorCallStrengthPreference mVibratorCallStrength;
+    public static final String KEY_NOTIF_VIBSTRENGTH = "vib_notif_strength";
+    private VibratorNotifStrengthPreference mVibratorNotifStrength;
+
+    public static final String KEY_FPS_INFO = "fps_info";
+    private static SwitchPreference mFpsInfo;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.main);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+
+        mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
+        if (mVibratorStrength != null)
+            mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
+        mVibratorCallStrength = (VibratorCallStrengthPreference) findPreference(KEY_CALL_VIBSTRENGTH);
+        if (mVibratorCallStrength != null)
+            mVibratorCallStrength.setEnabled(VibratorCallStrengthPreference.isSupported());
+        mVibratorNotifStrength = (VibratorNotifStrengthPreference) findPreference(KEY_NOTIF_VIBSTRENGTH);
+        if (mVibratorNotifStrength != null)
+            mVibratorNotifStrength.setEnabled(VibratorNotifStrengthPreference.isSupported());
+
+        mFpsInfo = (SwitchPreference) findPreference(KEY_FPS_INFO);
+        mFpsInfo.setChecked(prefs.getBoolean(KEY_FPS_INFO, false));
+        mFpsInfo.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFpsInfo) {
+            boolean enabled = (Boolean) newValue;
+            Intent fpsinfo = new Intent(this.getContext(), org.lineageos.device.DeviceSettings.FPSInfoService.class);
+            if (enabled) {
+                this.getContext().startService(fpsinfo);
+            } else {
+                this.getContext().stopService(fpsinfo);
+            }
+        }
+        else {
+            Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
