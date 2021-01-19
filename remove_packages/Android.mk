de9a1616fd8f9c0820295087d01b26ef1bfa1786
Aryan Arora <arora.aryan7@gmail.com>
2021-01-19 14:20:03 +0100

    Exclude more packages
    and Build Chrome

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions OemDmTrigger Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio PrebuiltGmail Snap Camera2 GoogleFeedback RecorderPrebuilt Videos Chrome Ornament Photos Tycho GoogleFeedback USCCDM AppDirectedSMSService ConnMO DCMO NgaResources
+LOCAL_OVERRIDES_PACKAGES :=  CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions OemDmTrigger Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio PrebuiltGmail Snap Camera2 GoogleFeedback RecorderPrebuilt Videos Ornament Photos Tycho GoogleFeedback USCCDM AppDirectedSMSService ConnMO DCMO NgaResources
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
