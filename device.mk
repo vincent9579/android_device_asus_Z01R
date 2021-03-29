9abf3852bb3fa7778845b4722b7c7fadc2f0f5c5
Michael Bestas <mkbestas@lineageos.org>
2021-03-29 21:03:42 +0300

    Z01R: Switch to AOSP NFC
    Change-Id: I0dea5ce8a9a2f4a7cce74824ef5e2e50e965242b

--- device.mk
+++ device.mk
@@ -368,15 +368,8 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     android.hardware.secure_element@1.2 \
     com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    libnqnfc_nci_jni \
-    nfc_nci.nqx.default.hw \
-    nqnfcee_access.xml \
-    NQNfcNci \
     NfcNci \
-    Tag \
-    vendor.nxp.hardware.nfc@2.0-service
+    Tag
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
@@ -435,8 +428,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/nxp/opensource/pn5xx
+    $(LOCAL_PATH)
 
 # Telephony
 PRODUCT_PACKAGES += \
