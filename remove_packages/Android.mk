ca5e00a253ec8d743631a29cc51cdfedb1419f03
AruAA <arora.aryan7@gmail.com>
2021-05-11 13:53:17 +0200

    Exclude Snap Cam

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt PrebuiltGmail GoogleFeedback RecorderPrebuilt Videos Ornament Photos Tycho GoogleFeedback NgaResources
+LOCAL_OVERRIDES_PACKAGES :=  Drive Maps Snap Camera2 MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt PrebuiltGmail GoogleFeedback RecorderPrebuilt Videos Ornament Photos Tycho GoogleFeedback NgaResources
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
